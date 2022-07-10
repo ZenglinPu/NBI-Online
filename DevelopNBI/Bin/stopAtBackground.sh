26bc119d4d47b729ef4e2512e2f27c6293a3c65c
Xiang Qingfeng <1638631691@qq.com>
2022-07-10 21:17:52 +0800

    add stopAtBackground.sh

--- DevelopNBI/Bin/stopAtBackground.sh
+++ DevelopNBI/Bin/stopAtBackground.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-# 先查找
-ps -aux | grep manage.py
+# 先查找可能的PID
+
+PIDs=`ps -ef | grep manage.py | grep -v grep | awk '{print $2}'`
+
+# Or find the PID manually
+# ps -ef | grep manage.py
 
 # 在杀掉进程就好
+for PID in $PIDs
+do
+    kill -9 $PID
+    echo "Kill $PID"
+done
+echo "All Killed"
