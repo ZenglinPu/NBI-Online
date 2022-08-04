ba949f37e6aa9e386dbc1d1d21509550359fe9df
ZenglinPu <43563763+ZenglinPu@users.noreply.github.com>
2022-08-04 22:02:40 +0800

    Delete DevelopNBI directory

--- DevelopNBI/Bin/startAtBackground.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-nohup python3 /home/ubuntu/DevelopNBI/manage.py runserver 0:7000>djo.out 2>&1 &
--- DevelopNBI/NBI/UserManagement/md5.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-
-# 返回md5加密后的字符串
-def transToMD5(pw):
-    ret = hashlib.md5(pw.encode(encoding='UTF-8')).hexdigest()
-    return ret
