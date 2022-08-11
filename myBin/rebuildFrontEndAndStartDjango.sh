f0a22d13c2722d06ff1219e11ccdded90ca167d7
root <root@localhost.localdomain>
2022-08-11 23:17:06 +0800

    little bug in bin

--- myBin/rebuildFrontEndAndStartDjango.sh
+++ myBin/rebuildFrontEndAndStartDjango.sh
@@ -2,7 +2,7 @@
 
 echo "Rebuild FrontEnd and Restart Django."
 CURRENT_DIR=$(cd $(dirname $0); pwd)
-cd ${CURRENT_DIR}/../frontend || exit
+cd ${CURRENT_DIR}/../front || exit
 npm run build
 
 bash ${CURRENT_DIR}/startTestServer.sh
