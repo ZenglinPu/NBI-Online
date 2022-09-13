cd02a95f4410c558e999e57fd54a28c139b56267
Xiang Qingfeng <1638631691@qq.com>
2022-09-13 10:37:58 +0800

    在manage.py中加入了GC的启动代码

--- NBIOnline/manage.py
+++ NBIOnline/manage.py
@@ -2,11 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import argparse
+
+from .NBIOnline.imageProcess.imageGCSchedule import GCTask
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--django_settings', required=False, help='Django settings module', default='NBIOnline.settings')
+parser.add_argument('--gc_mode', required=False, help='', default=False)
+parameters = parser.parse_args()
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NBIOnline.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', parameters.django_settings)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,4 +27,7 @@ def main():
 
 
 if __name__ == '__main__':
+    gc_task = GCTask(gc=parameters.gc_mode)
+    gc_task.start()
     main()
+    gc_task.shutdown()
