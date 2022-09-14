8cbe6951fccb9f289a8848afd21d569b1ac2fd43
Xiang Qingfeng <1638631691@qq.com>
2022-09-14 19:54:31 +0800

    配置文件项的小幅更新

--- NBIOnline/NBIOnline/imageProcess/ImageProcesser.py
+++ NBIOnline/NBIOnline/imageProcess/ImageProcesser.py
@@ -3,13 +3,15 @@ import cv2
 import rawpy
 from PIL import Image as pillowImage
 from .NBIGenerator import getNBIImage_full, getRandom, pillow2cv2, getNBIImage_easy
+from ..configLoader import nbi_conf
 
+conf = nbi_conf
 
 # 输入cv2类图片，直接保存压缩后图片并且在数据库中记录
 def compressImage(image, name, rate):
     cname = name.split("_")[0] + "_compress_" + name.split("_")[1]
     # 本来就不大的图片就不压缩
-    if image.size > 500000:
+    if image.size > conf.configs['image_temp_compress_threshold']:
         cv2.imwrite(r"../NBIOnline/static/Data/Temp/{name}".format(name=cname), image, [cv2.IMWRITE_JPEG_QUALITY, rate])
     else:
         cv2.imwrite(r"../NBIOnline/static/Data/Temp/{name}".format(name=cname), image)
@@ -22,7 +24,7 @@ def storeInputImage(image_blue, image_green, image_white):
     if str(image_blue).split(".")[-1].lower() == "jpg" or str(image_blue).split(".")[-1].lower() == "jpeg":
         image_blue_name = str(image_blue).split(".")[0] + getRandom() + '.jpg'
         image_blue = pillow2cv2(pillowImage.open(image_blue))
-        if image_blue.size > 300000:
+        if image_blue.size > conf.configs['image_storage_compress_threshold']:
             # 太大的图片略微压缩
             cv2.imwrite(r"../NBIOnline/static/Data/Blue/{name}".format(name=image_blue_name), image_blue,
                         [cv2.IMWRITE_JPEG_QUALITY, 60])
@@ -38,7 +40,7 @@ def storeInputImage(image_blue, image_green, image_white):
     if str(image_green).split(".")[-1].lower() == "jpg" or str(image_blue).split(".")[-1].lower() == "jpeg":
         image_green_name = str(image_green).split(".")[0] + getRandom() + '.jpg'
         image_green = pillow2cv2(pillowImage.open(image_green))
-        if image_green.size > 300000:
+        if image_green.size > conf.configs['image_storage_compress_threshold']:
             cv2.imwrite(r"../NBIOnline/static/Data/Green/{name}".format(name=image_green_name), image_green,
                         [cv2.IMWRITE_JPEG_QUALITY, 60])
         else:
@@ -54,7 +56,7 @@ def storeInputImage(image_blue, image_green, image_white):
         if str(image_white).split(".")[-1].lower() == "jpg" or str(image_white).split(".")[-1].lower() == "jpeg":
             image_white_name = str(image_white).split(".")[0] + getRandom() + '.jpg'
             image_white = pillow2cv2(pillowImage.open(image_white))
-            if image_white.size > 300000:
+            if image_white.size > conf.configs['image_storage_compress_threshold']:
                 cv2.imwrite(r"../NBIOnline/static/Data/White/{name}".format(name=image_white_name), image_white,
                             [cv2.IMWRITE_JPEG_QUALITY, 60])
             else:
@@ -63,6 +65,60 @@ def storeInputImage(image_blue, image_green, image_white):
         else:
             image_white_name = str(image_white).split(".")[0] + getRandom() + '.jpg'
             raw2jpg(image_white, "Green", image_white_name)
+
+    return image_blue_name, image_green_name, image_white_name
+
+
+# 直接指定图片路径，处理并储存到相应路径下
+# 这个函数在解压缩的检查之后被调用
+def transPackageImage(image_blue, image_green, image_white):
+    # print(image_green, image_blue)
+    # 将非jpg格式的数据转换成jpg存储
+    # jpg格式压缩存储
+    if str(image_blue).split(".")[-1].lower() == "jpg" or str(image_blue).split(".")[-1].lower() == "jpeg":
+        image_blue_name = str(image_blue.split('/')[-1]).split(".")[0] + getRandom() + '.jpg'
+        image_blue = pillow2cv2(pillowImage.open(image_blue))
+        if image_blue.size > conf.configs['image_storage_compress_threshold']:
+            # 太大的图片略微压缩
+            cv2.imwrite(r"../NBIOnline/static/Data/Blue/{name}".format(name=image_blue_name), image_blue,
+                        [cv2.IMWRITE_JPEG_QUALITY, 60])
+        else:
+            # 不大的就几乎不压缩
+            cv2.imwrite(r"../NBIOnline/static/Data/Blue/{name}".format(name=image_blue_name), image_blue,
+                        [cv2.IMWRITE_JPEG_QUALITY, 80])
+    else:
+        # 转换为Jpg格式再存储
+        image_blue_name = str(image_blue.split('/')[-1]).split(".")[0] + getRandom() + '.jpg'
+        raw2jpg_file(image_blue, "Blue", image_blue_name)
+
+    if str(image_green).split(".")[-1].lower() == "jpg" or str(image_blue).split(".")[-1].lower() == "jpeg":
+        image_green_name = str(image_green.split('/')[-1]).split(".")[0] + getRandom() + '.jpg'
+        image_green = pillow2cv2(pillowImage.open(image_green))
+        if image_green.size > conf.configs['image_storage_compress_threshold']:
+            cv2.imwrite(r"../NBIOnline/static/Data/Green/{name}".format(name=image_green_name), image_green,
+                        [cv2.IMWRITE_JPEG_QUALITY, 60])
+        else:
+            cv2.imwrite(r"../NBIOnline/static/Data/Green/{name}".format(name=image_green_name), image_green,
+                        [cv2.IMWRITE_JPEG_QUALITY, 80])
+    else:
+        image_green_name = str(image_green.split('/')[-1]).split(".")[0] + getRandom() + '.jpg'
+        raw2jpg_file(image_green, "Green", image_green_name)
+
+    if image_white is None:
+        image_white_name = None
+    else:
+        if str(image_white).split(".")[-1].lower() == "jpg" or str(image_white).split(".")[-1].lower() == "jpeg":
+            image_white_name = str(image_white.split('/')[-1]).split(".")[0] + getRandom() + '.jpg'
+            image_white = pillow2cv2(pillowImage.open(image_white))
+            if image_white.size > conf.configs['image_storage_compress_threshold']:
+                cv2.imwrite(r"../NBIOnline/static/Data/White/{name}".format(name=image_white_name), image_white,
+                            [cv2.IMWRITE_JPEG_QUALITY, 60])
+            else:
+                cv2.imwrite(r"../NBIOnline/static/Data/White/{name}".format(name=image_white_name), image_white,
+                            [cv2.IMWRITE_JPEG_QUALITY, 80])
+        else:
+            image_white_name = str(image_white.split('/')[-1]).split(".")[0] + getRandom() + '.jpg'
+            raw2jpg_file(image_white, "Green", image_white_name)
 
     return image_blue_name, image_green_name, image_white_name
 
